80f2c9c0b88219b20c43a9677b3194c6acde16ef
gitgitgitgotgotgot <gitgitgitgotgotgot@gmail.com>
2016-11-20 13:54:57 +0500

    edit

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo /etc/init.d/mysql start
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
